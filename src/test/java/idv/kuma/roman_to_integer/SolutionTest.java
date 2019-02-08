07a328db2ce7d5edac57af53279a009716e092d9
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-09 01:02:40 +0800

    temp commit

--- src/test/java/idv/kuma/roman_to_integer/SolutionTest.java
+++ src/test/java/idv/kuma/roman_to_integer/SolutionTest.java
@@ -11,4 +11,34 @@ public class SolutionTest {
 
         Assert.assertEquals(5, solution.romanToInt("V"));
     }
+
+    @Test
+    public void When_III_Then_3() throws Exception {
+        Solution solution = new Solution();
+
+        Assert.assertEquals(3, solution.romanToInt("III"));
+    }
+
+
+
+//    Input: "III"
+//    Output: 3
+//    Example 2:
+//
+//    Input: "IV"
+//    Output: 4
+//    Example 3:
+//
+//    Input: "IX"
+//    Output: 9
+//    Example 4:
+//
+//    Input: "LVIII"
+//    Output: 58
+//    Explanation: L = 50, V= 5, III = 3.
+//    Example 5:
+//
+//    Input: "MCMXCIV"
+//    Output: 1994
+//    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 }
