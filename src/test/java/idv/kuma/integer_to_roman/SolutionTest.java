8c1b40a3f660e942fd2d0d0027d4da1bbb7984ea
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-09 15:27:27 +0800

    1,2 ok

--- src/test/java/idv/kuma/integer_to_roman/SolutionTest.java
+++ src/test/java/idv/kuma/integer_to_roman/SolutionTest.java
@@ -1,17 +1,31 @@
 package idv.kuma.integer_to_roman;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+
+    }
+
     @Test
     public void When_1_Then_I() throws Exception {
+        runAndCheck(1, "I");
+    }
 
-        Solution solution = new Solution();
+    @Test
+    public void When_2_Then_II() throws Exception {
+        runAndCheck(2, "II");
+    }
 
-        Assert.assertEquals("I", solution.intToRoman(1));
+    private void runAndCheck(int num, String expect) {
+        Assert.assertEquals(expect, solution.intToRoman(num));
     }
 }
