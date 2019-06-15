f54b707044b64baeab591e6baf45b06f3c7778bb
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-16 00:22:12 +0800

    flower ok

--- src/main/java/idv/kuma/longest_common_prefix/Solution.java
+++ src/main/java/idv/kuma/longest_common_prefix/Solution.java
@@ -1,11 +1,31 @@
 package idv.kuma.longest_common_prefix;
 
+import java.util.Arrays;
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
 
+        String first = strs[0];
 
-        return "";
+        int length = first.length();
+
+        String result = "";
+
+
+        for (int i = 1; i <= length; i++) {
+            String subString = first.substring(0, i);
+
+            boolean isPresent = Arrays.stream(strs).allMatch(str -> str.startsWith(subString));
+
+            if (isPresent) {
+                result = subString;
+            } else {
+                return result;
+            }
+        }
+
+        return result;
 
     }
 }
--- src/test/java/idv/kuma/longest_common_prefix/SolutionTest.java
+++ src/test/java/idv/kuma/longest_common_prefix/SolutionTest.java
@@ -7,9 +7,12 @@ public class SolutionTest {
 
     @Test
     public void aaa_vvvb_Then_empty() {
-
         Assert.assertEquals("", createAndRun("aaa", "vvvb"));
+    }
 
+    @Test
+    public void flower_flow_flight_Then_fl() {
+        Assert.assertEquals("fl", createAndRun("flower", "flow", "flight"));
     }
 
     private String createAndRun(String... strs) {
