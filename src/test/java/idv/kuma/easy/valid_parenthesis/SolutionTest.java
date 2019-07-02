1854c6574c84708f4bb66ef7b43a3b319bdc566a
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-02 23:54:12 +0800

    simple tests ok

--- src/test/java/idv/kuma/easy/valid_parenthesis/SolutionTest.java
+++ src/test/java/idv/kuma/easy/valid_parenthesis/SolutionTest.java
@@ -6,11 +6,26 @@ import org.junit.Test;
 public class SolutionTest {
 
     @Test
-    public void True() {
+    public void Simple_True_Cases() {
         Solution solution = new Solution();
+
 
         Assert.assertTrue(solution.isValid("()"));
         Assert.assertTrue(solution.isValid("[]"));
+        Assert.assertTrue(solution.isValid("[]{}()"));
+        Assert.assertTrue(solution.isValid(""));
+        Assert.assertTrue(solution.isValid("([[{}]])"));
+
+    }
+
+
+    @Test
+    public void Simple_False_Cases() {
+        Solution solution = new Solution();
+
+        Assert.assertFalse(solution.isValid("{"));
+        Assert.assertFalse(solution.isValid("}{"));
+        Assert.assertFalse(solution.isValid("[{]}"));
 
     }
 }
