8262f9342ce5acca7df467a5a3dd08584f315d99
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-04 18:27:53 +0800

    0_empty ok

---
+++ src/main/java/idv/kuma/easy/merge_two_sorted_lists/Solution.java
@@ -0,0 +1,16 @@
+package idv.kuma.easy.merge_two_sorted_lists;
+
+import java.util.List;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode next1 = l1;
+        ListNode next2 = l2;
+
+
+
+
+        return new ListNode(0);
+    }
+}
