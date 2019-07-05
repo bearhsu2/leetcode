0d222a49f3552924e56d151ef6da22342ca4517a
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-05 10:33:34 +0800

    use reduce method

--- src/main/java/idv/kuma/hard/merge_k_sorted_lists/Solution.java
+++ src/main/java/idv/kuma/hard/merge_k_sorted_lists/Solution.java
@@ -1,42 +1,87 @@
 package idv.kuma.hard.merge_k_sorted_lists;
 
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
 
-
+        if (lists.length <= 0) return null;
 
         List<ListNode> listNodes = Arrays.asList(lists);
+
+
+        ListNode reduce = listNodes.stream()
+                .filter(l -> l != null)
+                .reduce((a, b) -> mergeTwoLists(a, b))
+                .orElse(null);
+
+        return reduce;
+    }
+
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode next1 = l1;
+        ListNode next2 = l2;
 
         ListNode head = new ListNode(-999);
         ListNode current = head;
 
-        Set<ListNode> finishedNodes = new HashSet<>();
-
         while (true) {
-            Optional<ListNode> minNodeOpt = listNodes.parallelStream()
-                    .filter(l -> l != null && !finishedNodes.contains(l))
-                    .min(Comparator.comparingInt(l -> l.val));
 
-            if (!minNodeOpt.isPresent()) break;
+            if (next1 != null && next2 != null) {
 
-            ListNode minNode = minNodeOpt.get();
+                int val1 = next1.val;
+                int val2 = next2.val;
 
-            ListNode newNode = new ListNode(minNode.val);
-            current.next = newNode;
-            current = current.next;
+                if (val1 < val2) {
+                    ListNode newNode = new ListNode(val1);
 
-            ListNode next = minNode.next;
+                    current.next = newNode;
+                    current = current.next;
 
-            if (null == next) {
-                finishedNodes.add(minNode);
+                    next1 = next1.next;
+                } else {
+
+                    ListNode newNode = new ListNode(val2);
+
+                    current.next = newNode;
+                    current = current.next;
+
+                    next2 = next2.next;
+
+                }
+
+
+            } else if (next1 == null && next2 != null) {
+
+                int val2 = next2.val;
+                ListNode newNode = new ListNode(val2);
+
+                current.next = newNode;
+                current = current.next;
+
+                next2 = next2.next;
+
+            } else if (next1 != null && next2 == null) {
+
+                int val1 = next1.val;
+                ListNode newNode = new ListNode(val1);
+
+                current.next = newNode;
+                current = current.next;
+
+                next1 = next1.next;
+
+
             } else {
-                minNode.val = next.val;
-                minNode.next = next.next;
+                break;
             }
+
         }
+
 
         return head.next;
     }
