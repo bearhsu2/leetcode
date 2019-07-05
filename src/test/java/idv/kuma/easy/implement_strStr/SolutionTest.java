b57b2ca6bdb87b3e6be85311a79c3bcf92714b6b
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-05 21:44:29 +0800

    refactor

--- src/test/java/idv/kuma/easy/implement_strStr/SolutionTest.java
+++ src/test/java/idv/kuma/easy/implement_strStr/SolutionTest.java
@@ -8,14 +8,18 @@ public class SolutionTest {
     @Test
     public void When_Empty_Then_0() {
 
-        Assert.assertEquals(0, new Solution().strStr("aaabbbccc", ""));
+        runAndCheck(0, "aaabbbccc", "");
 
+    }
+
+    private void runAndCheck(int i, String aaabbbccc, String s) {
+        Assert.assertEquals(i, new Solution().strStr(aaabbbccc, s));
     }
 
     @Test
     public void Given_hello_When_ll_Then_2() {
 
-        Assert.assertEquals(2, new Solution().strStr("hello", "ll"));
+        runAndCheck(2, "hello", "ll");
 
     }
 }
