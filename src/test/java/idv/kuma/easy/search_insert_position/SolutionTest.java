dd91b7f07262a35c258a347477b9b907d4f29f47
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-07 19:30:11 +0800

    refactor tests

--- src/test/java/idv/kuma/easy/search_insert_position/SolutionTest.java
+++ src/test/java/idv/kuma/easy/search_insert_position/SolutionTest.java
@@ -3,23 +3,39 @@ package idv.kuma.easy.search_insert_position;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SolutionTest {
 
     @Test
     public void Given_Empty_When_3_Then_0() {
 
-        Solution solution = new Solution();
-        int result = solution.searchInsert(new int[0], 3);
-        Assert.assertEquals(0, result);
+        runAndTest(new int[0], 3, 0);
     }
 
     @Test
     public void Given_1_2_4_5_When_3_Then_2() {
 
-        Solution solution = new Solution();
-        int result = solution.searchInsert(new int[]{1, 2, 4, 5}, 3);
-        Assert.assertEquals(2, result);
+        runAndTest(new int[]{1, 2, 4, 5}, 3, 2);
     }
+
+    private void runAndTest(int[] nums, int target, int expected) {
+        Solution solution = new Solution();
+        int result = solution.searchInsert(nums, target);
+        Assert.assertEquals(expected, result);
+    }
+
+
+//    Input: [1,3,5,6], 5
+//    Output: 2
+//    Example 2:
+//
+//    Input: [1,3,5,6], 2
+//    Output: 1
+//    Example 3:
+//
+//    Input: [1,3,5,6], 7
+//    Output: 4
+//    Example 4:
+//
+//    Input: [1,3,5,6], 0
+//    Output: 0
 }
