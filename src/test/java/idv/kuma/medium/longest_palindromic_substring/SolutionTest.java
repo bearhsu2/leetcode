a73890a0b4acdcd19351b7eb4c769a6ed015f50b
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-14 18:45:15 +0800

    empty => empty

--- src/test/java/idv/kuma/medium/longest_palindromic_substring/SolutionTest.java
+++ src/test/java/idv/kuma/medium/longest_palindromic_substring/SolutionTest.java
@@ -8,6 +8,12 @@ import static org.junit.Assert.*;
 public class SolutionTest {
 
     @Test
+    public void When_Empty_Then_Empty() {
+
+        Assert.assertEquals("", new Solution().longestPalindrome(""));
+    }
+
+    @Test
     public void When_a_Then_a() {
 
         Assert.assertEquals("a", new Solution().longestPalindrome("a"));
