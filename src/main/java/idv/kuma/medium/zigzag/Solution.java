81ed83fe4d5bc7eb5d7e503bc8cbc4f967c55f35
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-18 00:04:20 +0800

    real alghrothm

--- src/main/java/idv/kuma/medium/zigzag/Solution.java
+++ src/main/java/idv/kuma/medium/zigzag/Solution.java
@@ -1,7 +1,40 @@
 package idv.kuma.medium.zigzag;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public String convert(String s, int numRows) {
-        return "PAHNAPLSIIGYIR";
+
+
+        int direction = -1;
+
+        char[] chars = s.toCharArray();
+
+        List<StringBuilder> intermediate = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            intermediate.add(new StringBuilder());
+        }
+
+
+        int i = 0;
+        for (char oneChar : chars) {
+
+            System.out.println("i = " + i);
+
+            intermediate.get(i).append(oneChar);
+
+            if (i == numRows - 1 || i == 0) direction *= -1;
+
+            i += direction;
+        }
+
+        StringBuilder result = new StringBuilder();
+        intermediate.forEach( stringBuilder -> {
+            result.append(stringBuilder);
+        });
+
+        return result.toString();
     }
 }
