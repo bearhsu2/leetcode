0a6af53d4d2dfcfaa2ddd2d958a0b89c0620fb25
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-17 11:00:07 +0800

    a

--- src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
+++ src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 1; j <= s.length(); j++) {
 
-                if (j - i < longestLength) break;
+                if (j - i < longestLength) continue;
 
 
                 String currentSubstring = s.substring(i, j);
