e72609c5aea8d09e008ca0a1260823d7763ee78c
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-17 10:40:14 +0800

    local ok

--- src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
+++ src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
@@ -3,6 +3,44 @@ package idv.kuma.medium.longest_palindromic_substring;
 class Solution {
     public String longestPalindrome(String s) {
 
-        return s;
+        int longestLength = 0;
+        String longestSubstring = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+
+
+                String currentSubstring = s.substring(i, j);
+
+                if (isPallindromic(currentSubstring)) {
+
+                    int currentLength = currentSubstring.length();
+                    if (currentLength > longestLength) {
+                        longestLength = currentLength;
+                        longestSubstring = currentSubstring;
+                    }
+
+                }
+            }
+        }
+
+        return longestSubstring;
+    }
+
+    boolean isPallindromic(String s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+
     }
 }
--- src/test/java/idv/kuma/medium/longest_palindromic_substring/SolutionTest.java
+++ src/test/java/idv/kuma/medium/longest_palindromic_substring/SolutionTest.java
@@ -3,7 +3,7 @@ package idv.kuma.medium.longest_palindromic_substring;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.stream.IntStream;
 
 public class SolutionTest {
 
@@ -17,5 +17,61 @@ public class SolutionTest {
     public void When_a_Then_a() {
 
         Assert.assertEquals("a", new Solution().longestPalindrome("a"));
+    }
+
+    @Test
+    public void When_ab_Then_a() {
+
+        Assert.assertEquals("a", new Solution().longestPalindrome("ab"));
+    }
+
+    @Test
+    public void When_abba_Then_abba() {
+
+        Assert.assertEquals("a", new Solution().longestPalindrome("ab"));
+    }
+
+
+    @Test
+    public void When_abbab_Then_abba() {
+
+        Assert.assertEquals("a", new Solution().longestPalindrome("ab"));
+    }
+
+
+    @Test
+    public void When_SpecialString_Then_SpecialString() {
+
+        String s = makeSpecialString();
+        Assert.assertEquals(s, new Solution().longestPalindrome(s));
+    }
+
+
+    @Test
+    public void When_SpecialStringA_Then_SpecialString() {
+
+        String s = makeSpecialString();
+        Assert.assertEquals(s, new Solution().longestPalindrome(s + "aaaa"));
+    }
+
+    private String makeSpecialString() {
+        StringBuilder sb = new StringBuilder();
+        IntStream.rangeClosed(0, 333).forEach(i -> sb.append("a"));
+        IntStream.rangeClosed(0, 333).forEach(i -> sb.append("b"));
+        IntStream.rangeClosed(0, 333).forEach(i -> sb.append("a"));
+        return sb.toString();
+    }
+
+    @Test
+    public void Test_isPallindromic() {
+
+        Assert.assertTrue(new Solution().isPallindromic("a"));
+        Assert.assertTrue(new Solution().isPallindromic("aba"));
+        Assert.assertTrue(new Solution().isPallindromic("abba"));
+        Assert.assertTrue(new Solution().isPallindromic("aabcbaa"));
+        Assert.assertTrue(new Solution().isPallindromic("abccba"));
+
+        Assert.assertFalse(new Solution().isPallindromic("aabcba"));
+        Assert.assertFalse(new Solution().isPallindromic("aabcbffa"));
     }
 }
