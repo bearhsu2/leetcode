fa5033559f0b3d2b4124d7f63e459a6892e4e921
Yu-Song Syu <kuma@simbo.com.tw>
2019-12-22 20:23:26 +0800

    failed case

--- src/test/java/idv/kuma/easy/word_pattern/SolutionTest.java
+++ src/test/java/idv/kuma/easy/word_pattern/SolutionTest.java
@@ -1,7 +1,21 @@
 package idv.kuma.easy.word_pattern;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SolutionTest {
 
+    @Test
+    public void When_abba_With_dog_cat_cat_dog_Then_true() {
+
+        Solution solution = new Solution();
+
+        String pattern = "abba";
+        String str = "dog cat cat dog";
+        boolean actual = solution.wordPattern(pattern, str);
+
+        Assert.assertEquals(true, actual);
+
+
+    }
 }
