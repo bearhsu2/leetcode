2e9c142253cba3eb53ad44624aa2b249f59a6465
Yu-Song Syu <kuma@simbo.com.tw>
2019-12-22 20:20:26 +0800

    init

---
+++ src/main/java/idv/kuma/easy/word_pattern/Solution.java
@@ -0,0 +1,9 @@
+package idv.kuma.easy.word_pattern;
+
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+
+        return false;
+
+    }
+}
---
+++ src/test/java/idv/kuma/easy/word_pattern/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.word_pattern;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
