585639a3fe925d68bf2118bd5b5bd1e5443296bb
Yu-Song Syu <kuma@simbo.com.tw>
2020-01-27 20:59:49 +0800

    feasible solution

---
+++ src/main/java/idv/kuma/easy/valid_anagram/Solution.java
@@ -0,0 +1,49 @@
+package idv.kuma.easy.valid_anagram;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> characterToCount = new HashMap<>();
+
+
+        // phase 1: iterate s
+        for (char c : s.toCharArray()) {
+
+            Integer count = characterToCount.get(c);
+
+            if (Objects.isNull(count)) {
+                characterToCount.put(c, 1);
+            } else {
+                characterToCount.put(c, count + 1);
+            }
+
+        }
+
+        // phase 2: iterate t
+        for (char c : t.toCharArray()) {
+
+            Integer count = characterToCount.get(c);
+
+            if (Objects.isNull(count)) {
+                characterToCount.put(c, -1);
+            } else {
+                characterToCount.put(c, count - 1);
+            }
+
+        }
+
+
+        // check if map contains only 0
+        return characterToCount.values()
+                .stream()
+                .allMatch(count -> count == 0);
+    }
+}
