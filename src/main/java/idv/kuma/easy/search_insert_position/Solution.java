82b80dd7bb4bb21b91a1acc9e3fbc700f0847297
Yu-Song Syu <kuma@simbo.com.tw>
2020-01-28 13:13:17 +0800

    use binary search to solve "035"

--- src/main/java/idv/kuma/easy/search_insert_position/Solution.java
+++ src/main/java/idv/kuma/easy/search_insert_position/Solution.java
@@ -1,19 +1,41 @@
 package idv.kuma.easy.search_insert_position;
 
-import java.util.stream.IntStream;
-
 public class Solution {
     public int searchInsert(int[] nums, int target) {
+
+//        System.out.println("===========================================");
 
         int arrayLength = nums.length;
 
         if (arrayLength <= 0) return 0;
 
-        return IntStream.range(0, arrayLength)
-                .filter(index -> nums[index] >= target)
-                .findFirst()
-                .orElse(arrayLength);
 
+        int low = 0;
+        int high = arrayLength - 1;
+
+        while (low <= high) {
+
+
+            int middle = (low + high) / 2;
+
+//            System.out.println("low = " + low + ", middle = " + middle + ", high = " + high);
+
+
+            int middleValue = nums[middle];
+            if (middleValue == target) {
+                return middle;
+            }
+
+            if (middleValue < target) {
+                low = middle + 1;
+            } else { // middleValue > target
+                high = middle - 1;
+            }
+
+
+        }
+
+        return low;
 
     }
 }
