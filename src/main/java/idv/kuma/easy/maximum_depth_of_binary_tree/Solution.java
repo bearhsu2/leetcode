d9d01dd5a80b78d617a6d530cfb9e51ca3181773
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-05 00:44:56 +0800

    ok

---
+++ src/main/java/idv/kuma/easy/maximum_depth_of_binary_tree/Solution.java
@@ -0,0 +1,16 @@
+package idv.kuma.easy.maximum_depth_of_binary_tree;
+
+import java.util.Optional;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
+}
