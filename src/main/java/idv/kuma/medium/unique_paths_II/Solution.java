8d57951be88b2e9f386054fec04200d1bcc9a1d5
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-07 00:47:57 +0800

    refactor 63

--- src/main/java/idv/kuma/medium/unique_paths_II/Solution.java
+++ src/main/java/idv/kuma/medium/unique_paths_II/Solution.java
@@ -14,16 +14,19 @@ class Solution {
 
         int[][] dp = new int[m][n];
 
+        dp[0][0] = 1;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = (j == 0 ? 0 : dp[i][j - 1])
-                            + (i == 0 ? 0 : dp[i - 1][j]);
+                if (obstacleGrid[i][j] == 0) {
+
+                    if (i - 1 >= 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
                 }
 
             }
