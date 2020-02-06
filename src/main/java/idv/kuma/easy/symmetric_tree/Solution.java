741ab7134e2beef4a5a36312ae6034453c8a5e72
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-06 15:29:24 +0800

    add iterative solution to 101

--- src/main/java/idv/kuma/easy/symmetric_tree/Solution.java
+++ src/main/java/idv/kuma/easy/symmetric_tree/Solution.java
@@ -1,5 +1,8 @@
 package idv.kuma.easy.symmetric_tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
 
@@ -29,5 +32,48 @@ public class Solution {
 
         return areSymmetric(left.left, right.right)
                 && areSymmetric(left.right, right.left);
+    }
+}
+
+class IntrativeSolution {
+    public boolean isSymmetric(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        List<TreeNode> list = new ArrayList<>();
+
+        list.add(root.left);
+        list.add(root.right);
+
+
+        while (list.size() > 0) {
+
+            TreeNode left = list.remove(0);
+            TreeNode right = list.remove(0);
+
+            if (left == null && right == null) {
+                continue;
+            }
+
+            if (left == null || right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            list.add(left.left);
+            list.add(right.right);
+
+            list.add(left.right);
+            list.add(right.left);
+
+        }
+
+        return true;
+
     }
 }
