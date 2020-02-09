548578bf44c88ba4b65c0960d0a13e8730612d3a
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-09 10:25:38 +0800

    unify the for loop

--- src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
+++ src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
@@ -13,20 +13,14 @@ class Solution {
 
         boolean[][] isPalindrome = new boolean[length][length];
 
-        for (int i = 0; i < length; i++) {
-            isPalindrome[i][i] = true;
-            longest = s.substring(i, i + 1);
 
-        }
-
-
-        for (int tryLength = 2; tryLength <= length; tryLength++) {
+        for (int tryLength = 1; tryLength <= length; tryLength++) {
 
             for (int i = 0; i < length; i++) {
                 int j = i + tryLength - 1;
 
                 if (j < length) {
-                    if ((tryLength <= 3 || isPalindrome[i + 1][j - 1]) && chars[i] == chars[j]) {
+                    if ((i == j || tryLength <= 3 || isPalindrome[i + 1][j - 1]) && chars[i] == chars[j]) {
                         isPalindrome[i][j] = true;
                         longest = s.substring(i, i + tryLength);
                     }
