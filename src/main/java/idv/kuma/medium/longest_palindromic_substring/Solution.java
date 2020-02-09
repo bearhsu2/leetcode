c37f68d5f0583f79cce1969a81c1748aa1622d38
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-09 10:15:08 +0800

    ok

--- src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
+++ src/main/java/idv/kuma/medium/longest_palindromic_substring/Solution.java
@@ -20,29 +20,14 @@ class Solution {
 
         }
 
-        for (int tryLength = 2; tryLength <= 2; tryLength++) {
 
-            for (int i = 0; i < length; i++) {
-
-                int j = i + tryLength - 1;
-
-                if (j < length) {
-                    if (chars[i] == chars[j]) {
-                        isPalindrome[i][j] = true;
-                        currentLongestPalindrome = s.substring(i, i + tryLength);
-                    }
-                }
-            }
-        }
-
-        for (int tryLength = 3; tryLength <= length && tryLength > currentLongestPalindrome.length(); tryLength++) {
-
+        for (int tryLength = 2; tryLength <= length && tryLength > currentLongestPalindrome.length(); tryLength++) {
 
             for (int i = 0; i < length; i++) {
                 int j = i + tryLength - 1;
 
                 if (j < length) {
-                    if (isPalindrome[i + 1][j - 1] && chars[i] == chars[j]) {
+                    if ((tryLength <= 3 || isPalindrome[i + 1][j - 1]) && chars[i] == chars[j]) {
                         isPalindrome[i][j] = true;
                         currentLongestPalindrome = s.substring(i, i + tryLength);
                     }
