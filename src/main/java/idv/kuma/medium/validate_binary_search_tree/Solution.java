ea7c8f6801eefafff5a485426f9354298e6ace5c
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-16 00:59:13 +0800

    recursive ok

---
+++ src/main/java/idv/kuma/medium/validate_binary_search_tree/Solution.java
@@ -0,0 +1,31 @@
+package idv.kuma.medium.validate_binary_search_tree;
+
+public class Solution {
+    long last = Long.MIN_VALUE;
+
+
+    public boolean isValidBST(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+
+        long previousLast = last;
+        last = root.val;
+        if (root.val <= previousLast) {
+            return false;
+        }
+
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+
+        return true;
+
+    }
+}
+
---
+++ src/test/java/idv/kuma/medium/validate_binary_search_tree/SolutionTest.java
@@ -0,0 +1,63 @@
+package idv.kuma.medium.validate_binary_search_tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+
+    @Test
+    public void name00() {
+
+        TreeNode root = new TreeNode(Integer.MIN_VALUE);
+
+        Assert.assertTrue(new Solution().isValidBST(root));
+    }
+    @Test
+    public void name0() {
+
+        TreeNode root = new TreeNode(0);
+
+        TreeNode left = new TreeNode(-1);
+
+        root.left = left;
+
+        Assert.assertTrue(new Solution().isValidBST(root));
+    }
+
+    @Test
+    public void name() {
+
+        TreeNode root = new TreeNode(2);
+
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+
+        root.left = left;
+        root.right = right;
+
+        Assert.assertTrue(new Solution().isValidBST(root));
+    }
+
+
+    @Test
+    public void name2() {
+
+        TreeNode root = new TreeNode(5);
+
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(4);
+
+        root.left = left;
+        root.right = right;
+
+        TreeNode rightLeft = new TreeNode(3);
+        TreeNode rightRight = new TreeNode(6);
+
+        right.left = rightLeft;
+        right.right = rightRight;
+
+        Assert.assertFalse(new Solution().isValidBST(root));
+
+    }
+}
