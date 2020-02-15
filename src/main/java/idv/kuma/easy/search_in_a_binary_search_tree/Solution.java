e8ea3d10d8e3662d3f2f2dc2da6e448720ec3fe4
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-15 17:58:18 +0800

    refactor

--- src/main/java/idv/kuma/easy/search_in_a_binary_search_tree/Solution.java
+++ src/main/java/idv/kuma/easy/search_in_a_binary_search_tree/Solution.java
@@ -2,20 +2,18 @@ package idv.kuma.easy.search_in_a_binary_search_tree;
 
 public class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        return doRecursive(root, val);
-    }
-
-
-    private TreeNode doRecursive(TreeNode root, int val) {
         if (root == null || root.val == val) {
             return root;
         }
 
-        if (val < root.val) {
-            return doRecursive(root.left, val);
-        }
 
-        return doRecursive(root.right, val);
+        return searchBST(
+                val < root.val
+                        ? root.left
+                        : root.right
+                , val
+        );
+
     }
 
 
