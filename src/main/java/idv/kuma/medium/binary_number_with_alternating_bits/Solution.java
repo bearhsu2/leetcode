1a9c672e9fa56c7182ff2125ec752440f7304ce3
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-18 16:39:08 +0800

    refactor

--- src/main/java/idv/kuma/medium/binary_number_with_alternating_bits/Solution.java
+++ src/main/java/idv/kuma/medium/binary_number_with_alternating_bits/Solution.java
@@ -3,11 +3,9 @@ package idv.kuma.medium.binary_number_with_alternating_bits;
 public class Solution {
     public boolean hasAlternatingBits(int n) {
 
-        int xored = n >> 1 ^ n;
+        n = n >> 1 ^ n;
 
-        int added = xored + 1;
-
-        return (added & xored) == 0;
+        return (n + 1 & n) == 0;
 
     }
 }
