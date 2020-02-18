d9c6fb0b7e9f7c40929c42fd03f76c4b71c7a177
Yu-Song Syu <kuma@simbo.com.tw>
2020-02-18 16:34:51 +0800

    ok

--- src/main/java/idv/kuma/medium/binary_number_with_alternating_bits/Solution.java
+++ src/main/java/idv/kuma/medium/binary_number_with_alternating_bits/Solution.java
@@ -3,27 +3,11 @@ package idv.kuma.medium.binary_number_with_alternating_bits;
 public class Solution {
     public boolean hasAlternatingBits(int n) {
 
-        int shifted = n >> 1;
-
-        System.out.println(n);
-        System.out.println(shifted);
-
-        int xored = shifted ^ n;
-
-        System.out.println(xored);
+        int xored = n >> 1 ^ n;
 
         int added = xored + 1;
 
-        System.out.println(added);
-
-
-        int result = added & xored;
-
-        System.out.println(result);
-
-        System.out.println("--------");
-
-        return result == 0;
+        return (added & xored) == 0;
 
     }
 }
