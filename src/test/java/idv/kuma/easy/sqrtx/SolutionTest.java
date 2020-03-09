05dec7bb30db9ce4a3bf4c4a6a0c3336227592df
Yu-Song Syu <kuma@simbo.com.tw>
2020-03-10 00:32:59 +0800

    add new test cases

--- src/test/java/idv/kuma/easy/sqrtx/SolutionTest.java
+++ src/test/java/idv/kuma/easy/sqrtx/SolutionTest.java
@@ -6,6 +6,19 @@ import org.junit.Test;
 public class SolutionTest {
 
     @Test
+    public void big_numbers() {
+        Assertions.assertThat(new Solution().mySqrt(2147395599)).isEqualTo(46339);
+        Assertions.assertThat(new Solution().mySqrt(2147483647)).isEqualTo(46340);
+
+    }
+
+    @Test
+    public void _8192_Then_90() {
+
+        Assertions.assertThat(new Solution().mySqrt(8192)).isEqualTo(90);
+
+    }
+    @Test
     public void _8_Then_2() {
 
         Assertions.assertThat(new Solution().mySqrt(8)).isEqualTo(2);
