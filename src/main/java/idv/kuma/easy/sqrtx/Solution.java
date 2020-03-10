e09da9d57cca7df1d48191150f85ce9fb92937b6
Yu-Song Syu <kuma@simbo.com.tw>
2020-03-10 16:49:37 +0800

    consider input = 1 case

--- src/main/java/idv/kuma/easy/sqrtx/Solution.java
+++ src/main/java/idv/kuma/easy/sqrtx/Solution.java
@@ -3,8 +3,8 @@ package idv.kuma.easy.sqrtx;
 public class Solution {
     public int mySqrt(int x) {
 
-        if (x == 0) {
-            return 0;
+        if (x <= 1) {
+            return x;
         }
 
         return doSqrt(1, x / 2, x);
