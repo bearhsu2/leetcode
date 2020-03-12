8dc0a73493c23443d9d4d5e03f75d311368b33a3
Yu-Song Syu <kuma@simbo.com.tw>
2020-03-12 22:53:40 +0800

    1 ok

---
+++ src/main/java/idv/kuma/medium/perfect_square/Solution.java
@@ -0,0 +1,14 @@
+package idv.kuma.medium.perfect_square;
+
+public class Solution {
+
+    public int numSquares(int n) {
+
+        if (n == 1) return 1;
+
+
+
+        return -1;
+    }
+
+}
---
+++ src/test/java/idv/kuma/medium/perfect_square/SolutionTest.java
@@ -0,0 +1,29 @@
+package idv.kuma.medium.perfect_square;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void _1_Then_1() {
+        Assertions.assertThat(new Solution().numSquares(1)).isEqualTo(1);
+    }
+
+
+//    @Test
+//    public void _2_Then_2() {
+//    }
+//
+//
+//    @Test
+//    public void _4_Then_1() {
+//    }
+//
+//
+//    @Test
+//    public void _12_Then_3() {
+//    }
+}
