03701c75966b9b077bce47528fb3ede506b68660
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-06 15:58:19 +0800

    temp

---
+++ src/test/java/idv/kuma/easy/group_anagrams/SolutionTest.java
@@ -0,0 +1,46 @@
+package idv.kuma.easy.group_anagrams;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class SolutionTest {
+    @Test
+    public void _eat_tea_tan_ate_nat_bat() {
+
+        List<List<String>> lists =
+                new Solution()
+                        .groupAnagrams(
+                                new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+
+
+        System.out.println(lists);
+
+    }
+
+
+    @Test
+    public void _boo_bob() {
+
+        List<List<String>> lists =
+                new Solution()
+                        .groupAnagrams(
+                                new String[]{"boo", "bob"});
+        System.out.println(lists);
+
+    }
+
+
+    @Test
+    public void _zero_elements() {
+
+        List<List<String>> lists =
+                new Solution()
+                        .groupAnagrams(
+                                new String[]{});
+
+
+        System.out.println(lists);
+
+    }
+}
