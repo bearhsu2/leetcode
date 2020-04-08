e85ab6a97d96b5887026f44a248f98ea1aec40aa
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-08 20:26:44 +0800

    init

---
+++ src/main/java/idv/kuma/easy/middle_of_the_linked_list/Solution.java
@@ -0,0 +1,18 @@
+package idv.kuma.easy.middle_of_the_linked_list;
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        return null;
+    }
+}
