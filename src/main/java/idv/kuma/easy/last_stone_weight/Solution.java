2c3eb82211ef291cbe41ec254b6581af650c72cd
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-12 16:02:23 +0800

    1st try

--- src/main/java/idv/kuma/easy/last_stone_weight/Solution.java
+++ src/main/java/idv/kuma/easy/last_stone_weight/Solution.java
@@ -1,4 +1,34 @@
 package idv.kuma.easy.last_stone_weight;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+
+        Queue<Integer> queue = new PriorityQueue<>(1, (o1, o2) -> o2 - o1);
+
+
+        for (int stone : stones) {
+            queue.offer(stone);
+        }
+
+        while (queue.size() >= 2) {
+
+            int first = queue.poll();
+            int second = queue.poll();
+
+            int diff = first - second;
+
+            if (diff > 0) {
+                queue.offer(diff);
+            }
+
+        }
+
+        return queue.isEmpty()
+                ? 0
+                : queue.poll();
+    }
 }
--- src/test/java/idv/kuma/easy/last_stone_weight/SolutionTest.java
+++ src/test/java/idv/kuma/easy/last_stone_weight/SolutionTest.java
@@ -1,7 +1,15 @@
 package idv.kuma.easy.last_stone_weight;
 
-import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
 public class SolutionTest {
 
+    @Test
+    public void name() {
+
+        Assertions.assertThat(
+                new Solution().lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1}))
+                .isEqualTo(1);
+    }
 }
