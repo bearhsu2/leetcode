1ecb93f61d1458b8accc426c283f2aa35c6278a7
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-14 20:37:50 +0800

    remove extra blanks

--- src/main/java/idv/kuma/easy/perform_string_shifts/Solution.java
+++ src/main/java/idv/kuma/easy/perform_string_shifts/Solution.java
@@ -4,16 +4,11 @@ public class Solution {
 
     public String stringShift(String s, int[][] shifts) {
 
-
         int decimal = 0;
         for (int[] shift : shifts) {
-
             decimal += shift[1] * (shift[0] == 0 ? 1 : -1);
-
             decimal %= s.length();
-
         }
-
 
         if (decimal < 0) {
             decimal += s.length();
