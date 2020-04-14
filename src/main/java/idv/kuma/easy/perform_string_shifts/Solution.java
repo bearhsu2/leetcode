5c155fdf314531d0345cdf211b49b6055a98db1f
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-14 15:16:02 +0800

    init

---
+++ src/main/java/idv/kuma/easy/perform_string_shifts/Solution.java
@@ -0,0 +1,11 @@
+package idv.kuma.easy.perform_string_shifts;
+
+public class Solution {
+
+    public String stringShift(String s, int[][] shift) {
+
+        
+
+        return "";
+    }
+}
---
+++ src/test/java/idv/kuma/easy/perform_string_shifts/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.perform_string_shifts;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
