4a50e7a666ae6bceaa36c3f29ee0bd82675a918f
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-14 20:36:34 +0800

    AC

--- src/main/java/idv/kuma/easy/perform_string_shifts/Solution.java
+++ src/main/java/idv/kuma/easy/perform_string_shifts/Solution.java
@@ -5,15 +5,20 @@ public class Solution {
     public String stringShift(String s, int[][] shifts) {
 
 
-        int decimalShift = 0;
+        int decimal = 0;
         for (int[] shift : shifts) {
-            decimalShift += shift[1] * (shift[0] == 0 ? 1 : -1);
+
+            decimal += shift[1] * (shift[0] == 0 ? 1 : -1);
+
+            decimal %= s.length();
+
         }
 
-        
 
+        if (decimal < 0) {
+            decimal += s.length();
+        }
 
-
-        return "";
+        return s.substring(decimal) + s.substring(0, decimal);
     }
 }
