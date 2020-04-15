790bea893ca1a52acf0af31187ab4a579b002ed5
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-16 00:02:13 +0800

    AC

--- src/main/java/idv/kuma/medium/product_of_array_except_self/Solution.java
+++ src/main/java/idv/kuma/medium/product_of_array_except_self/Solution.java
@@ -1,8 +1,27 @@
 package idv.kuma.medium.product_of_array_except_self;
 
 public class Solution {
+    // Array, DP
     public int[] productExceptSelf(int[] nums) {
 
-        return nums;
+        int length = nums.length;
+        int maxIndex = length - 1;
+
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            L[i] = i == 0 ? 1 : L[i - 1] * nums[i - 1];
+
+            int j = maxIndex - i;
+            R[j] = j == maxIndex ? 1 : R[j + 1] * nums[j + 1];
+        }
+
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = L[i] * R[i];
+        }
+
+        return result;
     }
 }
