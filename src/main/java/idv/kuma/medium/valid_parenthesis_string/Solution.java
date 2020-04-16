120d5bb8c5a55c50d3a6bd625c283661f5c19e2f
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-16 15:57:24 +0800

    init

---
+++ src/main/java/idv/kuma/medium/valid_parenthesis_string/Solution.java
@@ -0,0 +1,8 @@
+package idv.kuma.medium.valid_parenthesis_string;
+
+public class Solution {
+    public boolean checkValidString(String s) {
+
+        return false;
+    }
+}
---
+++ src/test/java/idv/kuma/medium/valid_parenthesis_string/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.medium.valid_parenthesis_string;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
