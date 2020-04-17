cbd3f01ac4a105cdc15f7fa818e20093e3e637cd
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-17 15:29:22 +0800

    decide what to do

--- src/main/java/idv/kuma/medium/number_of_islands/Solution.java
+++ src/main/java/idv/kuma/medium/number_of_islands/Solution.java
@@ -3,8 +3,30 @@ package idv.kuma.medium.number_of_islands;
 public class Solution {
     public int numIslands(char[][] grid) {
 
-        int[][] handled = new int[grid.length][grid[0].length];
+        int number = 0;
 
-        return -1;
+        boolean[][] handled = new boolean[grid.length][grid[0].length];
+
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+
+                char c = grid[i][j];
+
+                if (c == '0') {
+                    handled[i][j] = true;
+                } else if (!handled[i][j]) {
+                    // find connected elements of c
+                    //   during which,
+                    //   all connected elements will be marked as handled
+
+
+                    // number ++
+                }
+
+            }
+        }
+
+        return number;
     }
 }
