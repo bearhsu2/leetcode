e44300091d1ad7785fa78fa764da35644c3a885b
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-18 16:07:04 +0800

    ac

--- src/main/java/idv/kuma/medium/minimum_path_sum/Solution.java
+++ src/main/java/idv/kuma/medium/minimum_path_sum/Solution.java
@@ -3,6 +3,34 @@ package idv.kuma.medium.minimum_path_sum;
 public class Solution {
     public int minPathSum(int[][] grid) {
 
-        return -1;
+        int height = grid.length;
+        if (height == 0) {
+            return 0;
+        }
+
+        int width = grid[0].length;
+        if (width == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[height][width];
+
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+
+        return dp[height - 1][width - 1];
     }
 }
