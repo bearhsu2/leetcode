769865d3618b5195b4b09176b4ca16b0b440011d
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-20 15:53:13 +0800

    init

---
+++ src/main/java/idv/kuma/medium/construct_binary_search_tree_from_preorder_traversal/Solution.java
@@ -0,0 +1,9 @@
+package idv.kuma.medium.construct_binary_search_tree_from_preorder_traversal;
+
+public class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+
+
+        return null;
+    }
+}
