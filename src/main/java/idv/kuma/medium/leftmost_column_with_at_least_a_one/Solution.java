18917fccad27a397bf74a1b7eed111801a669dbf
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-21 16:44:32 +0800

    init

---
+++ src/main/java/idv/kuma/medium/leftmost_column_with_at_least_a_one/Solution.java
@@ -0,0 +1,22 @@
+package idv.kuma.medium.leftmost_column_with_at_least_a_one;
+
+public class Solution {
+    // binary search
+
+
+    /**
+     * // This is the BinaryMatrix's API interface.
+     * // You should not implement it, or speculate about its implementation
+     * interface BinaryMatrix {
+     * public int get(int x, int y) {}
+     * public List<Integer> dimensions {}
+     * };
+     */
+
+
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+
+        return -1;
+    }
+
+}
---
+++ src/test/java/idv/kuma/medium/leftmost_column_with_at_least_a_one/SolutionTest.java
@@ -0,0 +1,50 @@
+package idv.kuma.medium.leftmost_column_with_at_least_a_one;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SolutionTest {
+
+    @Test
+    public void name() {
+        BinaryMatrix matrix = new DummyMatrix(new int[][]{
+                {0, 0},
+                {1, 1}
+        });
+
+        Assertions.assertThat(new Solution().leftMostColumnWithOne(matrix))
+                .isEqualTo(0);
+    }
+}
+
+class DummyMatrix implements BinaryMatrix {
+
+    private int[][] values;
+    private int n;
+    private int m;
+
+
+    public DummyMatrix(int[][] values) {
+
+        this.values = values;
+
+        n = values.length;
+        m = values[0].length;
+    }
+
+
+    @Override
+    public List<Integer> dimensions() {
+
+        return Arrays.asList(n, m);
+    }
+
+
+    @Override
+    public int get(int x, int y) {
+        return values[x][y];
+    }
+}
