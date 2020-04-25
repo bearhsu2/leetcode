c6e11630ea36912f865af81d2098fb5ccc4f1dd5
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-25 23:46:04 +0800

    ac

--- src/main/java/idv/kuma/medium/jump_game/Solution.java
+++ src/main/java/idv/kuma/medium/jump_game/Solution.java
@@ -3,6 +3,29 @@ package idv.kuma.medium.jump_game;
 public class Solution {
     public boolean canJump(int[] nums) {
 
+
+        int maxIndex = nums.length - 1;
+
+        int globalFarthest = 0;
+
+        for (int i = 0; i <= maxIndex; i++) {
+
+            if (globalFarthest < i) {
+                return false;
+            }
+
+            int localFarthest = i + nums[i];
+
+            if (localFarthest > globalFarthest) {
+                globalFarthest = localFarthest;
+            }
+
+            if (globalFarthest >= maxIndex) {
+                return true;
+            }
+
+        }
+
         return false;
     }
 }
