790ec63614c89e8909804aaf02c190f0395cc147
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-25 23:36:06 +0800

    ac

--- src/main/java/idv/kuma/medium/lru_cache/LRUCache.java
+++ src/main/java/idv/kuma/medium/lru_cache/LRUCache.java
@@ -1,20 +1,51 @@
 package idv.kuma.medium.lru_cache;
 
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class LRUCache {
 
-    public LRUCache(int capacity) {
+    private final int CAPACITY;
 
+    public Map<Integer, Integer> cache;
+
+
+    public LRUCache(int capacity) {
+        CAPACITY = capacity;
+        cache = new LinkedHashMap<>();
     }
 
 
     public int get(int key) {
 
-        return -1;
+        int value = -1;
+
+        if (cache.containsKey(key)) {
+
+            value = cache.get(key);
+
+            cache.remove(key);
+
+            cache.put(key, value);
+
+            return value;
+
+        }
+
+        return value;
     }
 
 
     public void put(int key, int value) {
+
+        if (cache.containsKey(key)) {
+            cache.remove(key);
+        } else if (cache.size() == CAPACITY) {
+            cache.remove(cache.keySet().iterator().next());
+        }
+        cache.put(key, value);
+
 
     }
 }
