2569c591c2fda6286dc0b610cca95dfe7842a1b5
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-27 23:48:26 +0800

    init

---
+++ src/main/java/idv/kuma/medium/maximal_square/Solution.java
@@ -0,0 +1,10 @@
+package idv.kuma.medium.maximal_square;
+
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+
+
+
+        return -1;
+    }
+}
---
+++ src/test/java/idv/kuma/medium/maximal_square/SolutionTest.java
@@ -0,0 +1,19 @@
+package idv.kuma.medium.maximal_square;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void name() {
+        char[][] matrix = new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}
+        };
+
+        Assertions.assertThat(new Solution().maximalSquare(matrix))
+                .isEqualTo(4);
+    }
+}
