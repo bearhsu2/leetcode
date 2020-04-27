9644a64f4c44c9c688913cd627fa958e0695538c
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-27 23:54:52 +0800

    use one-line-if

--- src/main/java/idv/kuma/medium/maximal_square/Solution.java
+++ src/main/java/idv/kuma/medium/maximal_square/Solution.java
@@ -20,11 +20,9 @@ public class Solution {
 
                 if (matrix[i][j] == '1') {
 
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-                    }
+                    dp[i][j] = i == 0 || j == 0
+                            ? 1
+                            : min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
 
                     maxSideLength = Math.max(maxSideLength, dp[i][j]);
 
