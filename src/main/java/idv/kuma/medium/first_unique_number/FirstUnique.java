bd59dfe0e13831fa5ba988727afb1695cdf32cb3
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-28 15:17:27 +0800

    temp

--- src/main/java/idv/kuma/medium/first_unique_number/FirstUnique.java
+++ src/main/java/idv/kuma/medium/first_unique_number/FirstUnique.java
@@ -1,8 +1,21 @@
 package idv.kuma.medium.first_unique_number;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class FirstUnique {
 
+    Queue<Integer> uniqueNumbers;
+
+
+
     public FirstUnique(int[] nums) {
+
+        this.uniqueNumbers = new ArrayDeque<>();
+
+        for (int num : nums) {
+            add(num);
+        }
 
     }
 
