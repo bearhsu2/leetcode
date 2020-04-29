9a2a1538b9c653166d7e51b900c9393b642d286d
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-29 11:06:32 +0800

    add failed cases

--- src/main/java/idv/kuma/medium/first_unique_number/FirstUnique.java
+++ src/main/java/idv/kuma/medium/first_unique_number/FirstUnique.java
@@ -1,16 +1,21 @@
 package idv.kuma.medium.first_unique_number;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Queue;
 
 class FirstUnique {
 
+
+    Map<Integer, Integer> appearedNumbers;
     Queue<Integer> uniqueNumbers;
 
 
 
     public FirstUnique(int[] nums) {
 
+        this.appearedNumbers = new HashMap<>();
         this.uniqueNumbers = new ArrayDeque<>();
 
         for (int num : nums) {
--- src/test/java/idv/kuma/medium/first_unique_number/FirstUniqueTest.java
+++ src/test/java/idv/kuma/medium/first_unique_number/FirstUniqueTest.java
@@ -1,7 +1,48 @@
 package idv.kuma.medium.first_unique_number;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class FirstUniqueTest {
 
+
+    public FirstUnique firstUnique;
+
+
+    @Test
+    public void name1() {
+
+
+        firstUnique = new FirstUnique(new int[]{2, 3, 5});
+        checkShowFirstUniqueEquals(2); // return 2
+
+        firstUnique.add(5);            // the queue is now [2,3,5,5]
+        checkShowFirstUniqueEquals(2);
+
+        firstUnique.add(2);            // the queue is now [2,3,5,5,2]
+        checkShowFirstUniqueEquals(3);
+
+        firstUnique.add(3);            // the queue is now [2,3,5,5,2,3]
+        checkShowFirstUniqueEquals(-1);
+
+    }
+
+
+    private void checkShowFirstUniqueEquals(int expected) {
+        Assert.assertEquals(expected, firstUnique.showFirstUnique());
+    }
+
+
+    @Test
+    public void name2() {
+        firstUnique = new FirstUnique(new int[]{7, 7, 7, 7, 7, 7});
+        checkShowFirstUniqueEquals(-1);
+
+        firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7]
+        firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3]
+        firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3,3]
+        firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7,3,3,7]
+        firstUnique.add(17);           // the queue is now [7,7,7,7,7,7,7,3,3,7,17]
+        checkShowFirstUniqueEquals(17);
+    }
 }
