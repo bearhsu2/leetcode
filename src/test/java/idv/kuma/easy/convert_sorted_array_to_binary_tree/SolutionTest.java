ead99ca3ffef202e0306ffa5335830a748565364
Yu-Song Syu <kuma@simbo.com.tw>
2020-05-01 12:00:00 +0800

    AC

--- src/test/java/idv/kuma/easy/convert_sorted_array_to_binary_tree/SolutionTest.java
+++ src/test/java/idv/kuma/easy/convert_sorted_array_to_binary_tree/SolutionTest.java
@@ -1,7 +1,16 @@
 package idv.kuma.easy.convert_sorted_array_to_binary_tree;
 
-import static org.junit.Assert.*;
+import idv.kuma.common.TreeNode;
+import org.junit.Test;
 
 public class SolutionTest {
+    @Test
+    public void name() {
+        int[] ints = new int[]{-10, -3, 0, 5, 9};
 
+        TreeNode root = new Solution().sortedArrayToBST(ints);
+
+
+        System.out.println("");
+    }
 }
