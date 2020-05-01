8d4ab029fab18ee2931829f3d01bd37c5563f6ab
Yu-Song Syu <kuma@simbo.com.tw>
2020-05-01 13:33:36 +0800

    ac

--- src/test/java/idv/kuma/easy/intersection_of_two_arrays_ii/SolutionTest.java
+++ src/test/java/idv/kuma/easy/intersection_of_two_arrays_ii/SolutionTest.java
@@ -1,7 +1,17 @@
 package idv.kuma.easy.intersection_of_two_arrays_ii;
 
-import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
 public class SolutionTest {
+    @Test
+    public void name() {
 
+        int[] nums1 = new int[]{1, 2, 2, 1};
+        int[] nums2 = new int[]{2, 2};
+
+        int[] result = new Solution().intersect(nums1, nums2);
+
+        Assertions.assertThat(result).containsExactlyInAnyOrder(2, 2);
+    }
 }
