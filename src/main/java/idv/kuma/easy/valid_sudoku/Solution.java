72f5400b75fb6f32e4f1b3041d502b10e72c6c85
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-10 16:07:57 +0800

    temp

--- src/main/java/idv/kuma/easy/valid_sudoku/Solution.java
+++ src/main/java/idv/kuma/easy/valid_sudoku/Solution.java
@@ -1,10 +1,45 @@
 package idv.kuma.easy.valid_sudoku;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
 
     public boolean isValidSudoku(char[][] board) {
+        boolean rows = checkRows(board);
 
+        boolean columns = checkcolumns(board);
+
+        boolean grids = checkGrids(board);
+
+        return rows && columns && grids;
+    }
+
+
+    private boolean checkGrids(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            char[] row = board[i];
+            if (!checkRow(row)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private boolean checkRow(char[] row) {
+
+        return false;
+    }
+
+
+    private boolean checkcolumns(char[][] board) {
+        return false;
+    }
+
+
+    private boolean checkRows(char[][] board) {
         return false;
     }
 
