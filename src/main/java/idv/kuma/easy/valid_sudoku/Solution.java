57d2ce63fbcfba3dc4d76c645c1607b84a40fa10
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-10 15:16:31 +0800

    init

---
+++ src/main/java/idv/kuma/easy/valid_sudoku/Solution.java
@@ -0,0 +1,11 @@
+package idv.kuma.easy.valid_sudoku;
+
+public class Solution {
+
+
+    public boolean isValidSudoku(char[][] board) {
+
+        return false;
+    }
+
+}
---
+++ src/test/java/idv/kuma/easy/valid_sudoku/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.valid_sudoku;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
