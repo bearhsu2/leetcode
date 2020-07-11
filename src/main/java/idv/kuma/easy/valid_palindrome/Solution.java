6727d9dd217b8320d833b403a7fa0cfcbbced999
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-11 12:52:03 +0800

    init

---
+++ src/main/java/idv/kuma/easy/valid_palindrome/Solution.java
@@ -0,0 +1,9 @@
+package idv.kuma.easy.valid_palindrome;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+
+
+        return false;
+    }
+}
---
+++ src/test/java/idv/kuma/easy/valid_palindrome/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.valid_palindrome;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
