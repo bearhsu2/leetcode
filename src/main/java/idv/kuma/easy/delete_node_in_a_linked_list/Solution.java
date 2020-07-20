51455b237074964f53b168e3bd2da80137d087af
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-20 23:20:44 +0800

    well...

--- src/main/java/idv/kuma/easy/delete_node_in_a_linked_list/Solution.java
+++ src/main/java/idv/kuma/easy/delete_node_in_a_linked_list/Solution.java
@@ -11,7 +11,8 @@ package idv.kuma.easy.delete_node_in_a_linked_list;
 class Solution {
     public void deleteNode(ListNode node) {
 
-        
+        node.val = node.next.val;
+        node.next = node.next.next;
 
     }
 }
