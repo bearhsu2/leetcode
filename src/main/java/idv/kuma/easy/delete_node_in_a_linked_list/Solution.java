5f241aa3ea9af072efdc8f3a7f9f149c0d41fa84
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-20 23:16:14 +0800

    init

---
+++ src/main/java/idv/kuma/easy/delete_node_in_a_linked_list/Solution.java
@@ -0,0 +1,17 @@
+package idv.kuma.easy.delete_node_in_a_linked_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        
+
+    }
+}
---
+++ src/test/java/idv/kuma/easy/delete_node_in_a_linked_list/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.delete_node_in_a_linked_list;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
