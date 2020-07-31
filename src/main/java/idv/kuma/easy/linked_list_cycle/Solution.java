a4a59f0511bf1b656538b146c5edc7294c4ea205
Yu-Song Syu <kuma@simbo.com.tw>
2020-07-31 15:04:46 +0800

    ac

--- src/main/java/idv/kuma/easy/linked_list_cycle/Solution.java
+++ src/main/java/idv/kuma/easy/linked_list_cycle/Solution.java
@@ -1,8 +1,24 @@
 package idv.kuma.easy.linked_list_cycle;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
+
+        ListNode current = head;
+
+        Set<ListNode> visited = new HashSet<>();
+
+        while (current != null) {
+            if (visited.contains(current)) {
+                return true;
+            }
+            visited.add(current);
+            current = current.next;
+        }
+
 
         return false;
     }
