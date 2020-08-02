c56a64d0ca4fef39ebc9978818f5044f65019582
Yu-Song Syu <kuma@simbo.com.tw>
2020-08-02 16:04:15 +0800

    init

---
+++ src/main/java/idv/kuma/easy/validate_binary_search_tree/Solution.java
@@ -0,0 +1,11 @@
+package idv.kuma.easy.validate_binary_search_tree;
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        return true;
+
+
+    }
+}
+
---
+++ src/test/java/idv/kuma/easy/validate_binary_search_tree/SolutionTest.java
@@ -0,0 +1,33 @@
+package idv.kuma.easy.validate_binary_search_tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void Single_Point() {
+
+        TreeNode head = new TreeNode(3);
+
+        Assert.assertTrue(
+                new Solution().isValidBST(head)
+        );
+
+    }
+
+
+    @Test
+    public void Has_Smaller_Left_Then_True() {
+
+        TreeNode head = new TreeNode(3);
+
+
+        TreeNode left = new TreeNode(2);
+        head.left = left;
+
+        Assert.assertTrue(
+                new Solution().isValidBST(head)
+        );
+
+    }
+}
