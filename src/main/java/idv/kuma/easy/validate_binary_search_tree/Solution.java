bd8758d4961557f16d49bff1066cbaebf8aaa019
Yu-Song Syu <kuma@simbo.com.tw>
2020-08-02 16:41:37 +0800

    ac

--- src/main/java/idv/kuma/easy/validate_binary_search_tree/Solution.java
+++ src/main/java/idv/kuma/easy/validate_binary_search_tree/Solution.java
@@ -3,26 +3,41 @@ package idv.kuma.easy.validate_binary_search_tree;
 public class Solution {
     public boolean isValidBST(TreeNode root) {
 
+        return doValid(root, null, null);
+
+
+    }
+
+
+    private boolean doValid(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+
+
+        if (min != null && root.val <= min) {
+            return false;
+        }
+
+        if (max != null && root.val >= max) {
+            return false;
+        }
+
 
         if (root.left != null) {
-
-            if (root.left.val >= root.val || !isValidBST(root.left)) {
+            if (root.left.val >= root.val || !doValid(root.left, min, root.val)) {
                 return false;
             }
 
         }
-
 
         if (root.right != null) {
-            if (root.right.val <= root.val) {
+            if (root.right.val <= root.val || !doValid(root.right, root.val, max)) {
                 return false;
             }
         }
 
-
         return true;
-
-
     }
 }
 
