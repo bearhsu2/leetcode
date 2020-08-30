e4522ccf6ddee329cdaa9c1fd3af8e3301b8e180
Yu-Song Syu <kuma@simbo.com.tw>
2020-08-30 17:16:10 +0800

    remove useless method

--- src/main/java/idv/kuma/easy/count_primes/Solution.java
+++ src/main/java/idv/kuma/easy/count_primes/Solution.java
@@ -34,20 +34,4 @@ class Solution {
     }
 
 
-    private boolean isPrime(int number) {
-
-        if (number <= 1) {
-            return false;
-        }
-
-        for (int i = 2; i * i <= number; i++) {
-
-            if (number % i == 0) {
-                return false;
-            }
-
-        }
-
-        return true;
-    }
 }
