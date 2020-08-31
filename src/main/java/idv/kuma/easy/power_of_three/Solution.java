d4e3b30dc1c5cb88230de944c3c9964b25180b61
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-01 00:06:38 +0800

    ac

--- src/main/java/idv/kuma/easy/power_of_three/Solution.java
+++ src/main/java/idv/kuma/easy/power_of_three/Solution.java
@@ -4,6 +4,22 @@ public class Solution {
 
 
     public boolean isPowerOfThree(int n) {
-        return false;
+
+        if (n <= 0) {
+            return false;
+        }
+
+
+        while (n != 1) {
+            double next = n / 3.0;
+
+            double floor = Math.floor(next);
+            if (next - floor > 0) {
+                return false;
+            }
+            n = (int) floor;
+        }
+
+        return true;
     }
 }
--- src/test/java/idv/kuma/easy/power_of_three/SolutionTest.java
+++ src/test/java/idv/kuma/easy/power_of_three/SolutionTest.java
@@ -6,13 +6,23 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void _27() {
-
-
         runAndCheck(true, 27);
     }
 
 
     private void runAndCheck(boolean expected, int number) {
         Assert.assertEquals(expected, new Solution().isPowerOfThree(number));
+    }
+
+
+    @Test
+    public void n3() {
+        runAndCheck(false, -3);
+    }
+
+
+    @Test
+    public void _45() {
+        runAndCheck(false, 45);
     }
 }
