6b401b5d7058c05f43f0c0904e77fa2bbb1a8623
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-01 00:10:09 +0800

    Math solution

--- src/main/java/idv/kuma/easy/power_of_three/Solution.java
+++ src/main/java/idv/kuma/easy/power_of_three/Solution.java
@@ -5,21 +5,7 @@ public class Solution {
 
     public boolean isPowerOfThree(int n) {
 
-        if (n <= 0) {
-            return false;
-        }
+        return (Math.log10(n) / Math.log10(3) % 1) == 0;
 
-
-        while (n != 1) {
-            double next = n / 3.0;
-
-            double floor = Math.floor(next);
-            if (next - floor > 0) {
-                return false;
-            }
-            n = (int) floor;
-        }
-
-        return true;
     }
 }
