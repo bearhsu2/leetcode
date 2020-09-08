a811854fb39b6cb61f66e5e81e55a81d4b059cb0
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-08 14:33:10 +0800

    ac

--- src/main/java/idv/kuma/easy/missing_number/Solution.java
+++ src/main/java/idv/kuma/easy/missing_number/Solution.java
@@ -1,10 +1,20 @@
 package idv.kuma.easy.missing_number;
 
+import java.util.stream.IntStream;
+
 public class Solution {
 
 
     public int missingNumber(int[] nums) {
 
-        return -1;
+        int sum = 
+
+        int actualSum = IntStream.of(nums).sum();
+
+
+        int expectedSum = (nums.length) * (nums.length + 1) / 2;
+
+
+        return expectedSum - actualSum;
     }
 }
