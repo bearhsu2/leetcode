935d9472e1629ce6cf7eea5dcf2de3033b45d05d
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-27 17:11:12 +0800

    add utils for tests

--- src/test/java/idv/kuma/medium/odd_even_linked_list/SolutionTest.java
+++ src/test/java/idv/kuma/medium/odd_even_linked_list/SolutionTest.java
@@ -1,7 +1,51 @@
 package idv.kuma.medium.odd_even_linked_list;
 
-import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SolutionTest {
+    @Test
+    public void _1_2_3_4__1_3_2_4() {
+
+        ListNode head = makeList(1, 2, 3, 4);
+
+        ListNode actual = new Solution().oddEvenList(head);
+
+        Assertions.assertThat(goThrough(actual)).containsExactly(1, 3, 2, 4);
+
+
+    }
+
+
+    private ListNode makeList(int... values) {
+
+        ListNode dummyHead = new ListNode(-999);
+        ListNode current = dummyHead;
+
+        for (int value : values) {
+            current.next = new ListNode(value);
+            current = current.next;
+        }
+
+        return dummyHead.next;
+
+    }
+
+
+    private List<Integer> goThrough(ListNode head) {
+        List<Integer> result = new ArrayList<>();
+
+        ListNode current = head;
+        while (current != null) {
+            result.add(current.val);
+            current = current.next;
+        }
+
+        return result;
+    }
+
 
 }
