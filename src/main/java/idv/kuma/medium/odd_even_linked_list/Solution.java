52a83574225716300e0f1cd658a10a523b88cd2c
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-27 17:01:20 +0800

    init

---
+++ src/main/java/idv/kuma/medium/odd_even_linked_list/Solution.java
@@ -0,0 +1,18 @@
+package idv.kuma.medium.odd_even_linked_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+    }
+}
+
