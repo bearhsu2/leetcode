61060464c5d40c6be7a63ae6c0f52a8f68e94973
Yu-Song Syu <kuma@simbo.com.tw>
2020-09-28 15:59:56 +0800

    add comment

--- src/main/java/idv/kuma/easy/intersection_of_two_linked_lists/Solution.java
+++ src/main/java/idv/kuma/easy/intersection_of_two_linked_lists/Solution.java
@@ -4,16 +4,19 @@ package idv.kuma.easy.intersection_of_two_linked_lists;
 public class Solution {
 
 
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
         if (headA == null || headB == null) {
             return null;
         }
 
-
         ListNode currentA = headA;
         ListNode currentB = headB;
 
+        // When currentA == currentB, there may be two situations:
+        // 1) A and B comes to a null simultaneously => no interaction
+        // 2) A and B comes to the same point => this is THE interaction
         while (currentA != currentB) {
 
             if (currentA == null) {
