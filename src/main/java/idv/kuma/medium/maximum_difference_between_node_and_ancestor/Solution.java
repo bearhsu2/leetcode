9fa344e7ce1a205670995c20862495f8488fa5f4
Yu-Song Syu <kuma@simbo.com.tw>
2020-11-12 23:20:49 +0800

    reformat

--- src/main/java/idv/kuma/medium/maximum_difference_between_node_and_ancestor/Solution.java
+++ src/main/java/idv/kuma/medium/maximum_difference_between_node_and_ancestor/Solution.java
@@ -1,24 +1,12 @@
 package idv.kuma.medium.maximum_difference_between_node_and_ancestor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Solution {
-
-    Map<TreeNode, Integer> nodeToMin = new HashMap<>();
-    Map<TreeNode, Integer> nodeToMax = new HashMap<>();
-
-    List<TreeNode> nodes = new ArrayList<>();
 
     int maxDiff = Integer.MIN_VALUE;
 
 
     int maxAncestorDiff(TreeNode root) {
-
         doTraverse(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
-
         return maxDiff;
     }
 
@@ -29,10 +17,8 @@ public class Solution {
             return;
         }
 
-
         int newMin = Math.min(node.val, parentMin);
         int newMax = Math.max(node.val, parentMax);
-
         int diff = Math.abs(newMin - newMax);
 
         maxDiff = Math.max(maxDiff, diff);
