9faa5506e636828834c4057285061cdf12912c69
Yu-Song Syu <kuma@cloud-latitude.com>
2022-02-24 22:49:22 +0800

    AC, ugly

--- src/main/java/idv/kuma/medium/remove_covered_intervals/Solution.java
+++ src/main/java/idv/kuma/medium/remove_covered_intervals/Solution.java
@@ -1,8 +1,46 @@
 package idv.kuma.medium.remove_covered_intervals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
+//        List<int[]> sorted = Arrays.stream(intervals)
+//                .sorted(Comparator.comparingInt(a -> a[0]))
+//                .collect(Collectors.toList());
 
-        return 0;
+        Arrays.sort(intervals, (a, b) -> {
+            int lResult = a[0] - b[0];
+            if (lResult != 0) {
+                return lResult;
+            }
+            return b[1] - a[1];
+        });
+
+        List<int[]> sorted = Arrays.stream(intervals).collect(Collectors.toList());
+        int result = sorted.size();
+
+        Set<Integer> removedIndex = new HashSet<>();
+        for (int i = 0; i < sorted.size() - 1; i++) {
+            if (removedIndex.contains(i)) continue;
+
+            int[] current = sorted.get(i);
+
+            for (int j = i + 1; j < sorted.size() ; j++) {
+                int[] next = sorted.get(j);
+                if (next[1] <= current[1]) {
+                    result--;
+                    removedIndex.add(j);
+                } else {
+                    break;
+                }
+            }
+        }
+
+
+        return result;
     }
 }
--- src/test/java/idv/kuma/medium/remove_covered_intervals/SolutionTest.java
+++ src/test/java/idv/kuma/medium/remove_covered_intervals/SolutionTest.java
@@ -6,12 +6,32 @@ import org.junit.Test;
 public class SolutionTest {
 
     @Test
-    public void name() {
+    public void case1() {
 
         int[][] intervals = {{1, 4}, {3, 6}, {2, 8}};
 
         int actual = new Solution().removeCoveredIntervals(intervals);
 
         Assert.assertEquals(2, actual);
+    }
+
+    @Test
+    public void case2() {
+
+        int[][] intervals = {{3, 10}, {4, 10}, {5, 11}};
+
+        int actual = new Solution().removeCoveredIntervals(intervals);
+
+        Assert.assertEquals(2, actual);
+    }
+
+    @Test
+    public void case3() {
+
+        int[][] intervals = {{1, 2}, {1, 4}, {3, 4}};
+
+        int actual = new Solution().removeCoveredIntervals(intervals);
+
+        Assert.assertEquals(1, actual);
     }
 }
