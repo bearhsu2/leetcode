4c404487e834a6c53e1e73241bb144740b5bf5ff
Yu-Song Syu <kuma@cloud-latitude.com>
2022-10-12 12:49:22 +0800

    perimeter

--- src/main/java/idv/kuma/easy/largest_perimeter_triangle/Solution.java
+++ src/main/java/idv/kuma/easy/largest_perimeter_triangle/Solution.java
@@ -1,8 +1,23 @@
 package idv.kuma.easy.largest_perimeter_triangle;
 
+import java.util.Arrays;
+
 class Solution {
     public int largestPerimeter(int[] nums) {
 
-        return 5;
+        int[] sorted = Arrays.stream(nums).sorted().toArray();
+
+
+        for (int i = sorted.length - 1; i >= 2; i--) {
+
+            if (sorted[i] < sorted[i - 1] + sorted[i - 2]) {
+                return sorted[i] + sorted[i - 1] + sorted[i - 2];
+            }
+
+        }
+
+        return 0;
+
+
     }
 }
--- src/test/java/idv/kuma/easy/largest_perimeter_triangle/SolutionTest.java
+++ src/test/java/idv/kuma/easy/largest_perimeter_triangle/SolutionTest.java
@@ -3,11 +3,14 @@ package idv.kuma.easy.largest_perimeter_triangle;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class SolutionTest {
     @Test
     public void _2_1_2() {
         Solution solution = new Solution();
         int actual = solution.largestPerimeter(new int[]{2, 1, 2});
+
 
         Assertions.assertThat(actual).isEqualTo(5);
     }
