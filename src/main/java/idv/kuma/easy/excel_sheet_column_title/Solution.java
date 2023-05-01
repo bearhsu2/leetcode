a9b8a63e6939f43ed435956065a584ab6f019e48
Yu-Song Syu <kuma@cloud-latitude.com>
2023-05-01 22:48:13 +0800

    extract field

--- src/main/java/idv/kuma/easy/excel_sheet_column_title/Solution.java
+++ src/main/java/idv/kuma/easy/excel_sheet_column_title/Solution.java
@@ -9,28 +9,29 @@ package idv.kuma.easy.excel_sheet_column_title;
 //AB -> 28
 //...
 class Solution {
+
+    private final char base = 'A';
+
     public String convertToTitle(int columnNumber) {
 
-        char base = 'A';
         int diff = columnNumber - 1; // 'A' maps to 1
 
 
         String resultStr = "";
         if (diff < 26) {
-            char currChar = getOneChar(base, diff);
+            char currChar = getOneChar(diff);
             return resultStr + currChar;
         } else {
             resultStr += "A";
             diff -= 26;
-            char currChar = getOneChar(base, diff);
+            char currChar = getOneChar(diff);
             return resultStr + currChar;
         }
 
 
     }
 
-    private char getOneChar(char base, int diff) {
-        char currChar = (char) (base + diff);
-        return currChar;
+    private char getOneChar(int diff) {
+        return (char) (base + diff);
     }
 }
