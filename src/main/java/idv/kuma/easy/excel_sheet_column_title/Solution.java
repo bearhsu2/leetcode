f701a6ee04f4d2d24e5d21a69f05f217c5c29fa0
Yu-Song Syu <kuma@cloud-latitude.com>
2023-05-01 22:25:17 +0800

    basic shape

---
+++ src/main/java/idv/kuma/easy/excel_sheet_column_title/Solution.java
@@ -0,0 +1,16 @@
+package idv.kuma.easy.excel_sheet_column_title;
+
+//A -> 1
+//B -> 2
+//C -> 3
+//...
+//Z -> 26
+//AA -> 27
+//AB -> 28
+//...
+class Solution {
+    public String convertToTitle(int columnNumber) {
+
+        return "";
+    }
+}
---
+++ src/test/java/idv/kuma/easy/excel_sheet_column_title/SolutionTest.java
@@ -0,0 +1,7 @@
+package idv.kuma.easy.excel_sheet_column_title;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+}
