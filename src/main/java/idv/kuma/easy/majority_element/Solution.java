7caa2a016747d27563fa26351c694710f06500a0
Yu-Song Syu <kuma@cloud-latitude.com>
2023-06-24 16:50:30 +0800

    temp

--- src/main/java/idv/kuma/easy/majority_element/Solution.java
+++ src/main/java/idv/kuma/easy/majority_element/Solution.java
@@ -1,9 +1,33 @@
 package idv.kuma.easy.majority_element;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int majorityElement(int[] nums) {
 
-        return -1;
+        int half = nums.length / 2;
+        Map<Integer, Integer> elementToCount = new HashMap<>();
+
+        for (int num : nums) {
+
+            Integer count = elementToCount.get(num);
+
+            if (count == null) {
+                count = 0;
+            }
+
+            count++;
+
+            if (count > half) {
+                return num;
+            }
+
+            elementToCount.put(num, count);
+
+        }
+
+        throw new RuntimeException("should not run this line");
 
     }
 }
