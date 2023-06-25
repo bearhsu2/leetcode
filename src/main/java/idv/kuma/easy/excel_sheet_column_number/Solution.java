d2220b40d7df7008e6ea5912d30ef7f3fa85c8fd
Yu-Song Syu <kuma@cloud-latitude.com>
2023-06-25 23:06:19 +0800

    AC

--- src/main/java/idv/kuma/easy/excel_sheet_column_number/Solution.java
+++ src/main/java/idv/kuma/easy/excel_sheet_column_number/Solution.java
@@ -2,9 +2,21 @@ package idv.kuma.easy.excel_sheet_column_number;
 
 public class Solution {
 
-    private final char base = 'A';
+    private final char base = 'A' - 1;
+
     public int titleToNumber(String columnTitle) {
 
-        return -1;
+        char[] charArray = columnTitle.toCharArray();
+
+        int result = 0;
+        for (char c : charArray) {
+
+            int diff = c - base;
+
+            result = result * 26 + diff;
+
+        }
+
+        return result;
     }
 }
--- src/test/java/idv/kuma/easy/excel_sheet_column_number/SolutionTest.java
+++ src/test/java/idv/kuma/easy/excel_sheet_column_number/SolutionTest.java
@@ -1,5 +1,6 @@
 package idv.kuma.easy.excel_sheet_column_number;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class SolutionTest {
@@ -7,5 +8,22 @@ public class SolutionTest {
 
     @Test
     public void A_1() {
+
+        int actual = new Solution().titleToNumber("A");
+        Assertions.assertThat(actual).isEqualTo(1);
+    }
+
+    @Test
+    public void AB_28() {
+
+        int actual = new Solution().titleToNumber("AB");
+        Assertions.assertThat(actual).isEqualTo(28);
+    }
+
+    @Test
+    public void ZY_701() {
+
+        int actual = new Solution().titleToNumber("ZY");
+        Assertions.assertThat(actual).isEqualTo(701);
     }
 }
